f3cd85d4f2d2ea4c2b7e098adbac2bb5a4fb44c5
smathews5 <smathews@myseneca.ca>
2021-03-15 19:48:11 -0400

    added header and navigation component

---
+++ src/components/Header.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Header = () => {
+    return (
+        <>
+        <div>
+
+<section id="first-section" className="bannerlabel">World of Games</section>
+        </div>
+
+      
+        </>
+    )
+}
+
+export default Header
---
+++ src/components/Navigation.js
@@ -0,0 +1,35 @@
+import React from 'react'
+
+const Navigation = () => {
+    return (
+        <>
+        <div>
+        <header>
+        <nav>
+        <ul>
+            <li><a href="/">Home </a></li>
+            <li><a href="/"> Games</a></li>
+            <li><a href="/profilePage">Login/SignUp </a></li>
+            <li><a href="">Contact </a> </li>
+
+        </ul>
+    </nav>
+    </header>
+        </div>
+        <div>
+        <nav>
+        <ul>
+            <li>
+                <a href="/"> Public Page</a>
+            </li>
+            <li>
+                <a href="/profilePage"> Profile Page</a>
+            </li>
+        </ul>
+        </nav>
+    </div>
+        </>
+    )
+}
+
+export default Navigation
