4321b79e22eedb88c606dd5ebf0c943179e3b7f2
smathews5 <smathews@myseneca.ca>
2021-03-15 19:36:43 -0400

    testing git

--- src/components/GameList.js
+++ src/components/GameList.js
@@ -6,6 +6,7 @@ import GameContext from "../context/GameContext";
 const GameList = () => {
     const {games,deleteGame} = useContext(GameContext);
     return (
+        //To check
         <section className="game-list-continer">
             {games.map((game)=>{
                 return <GameItem key={game.gameNum} item={game} onDeleteGame={deleteGame} />
