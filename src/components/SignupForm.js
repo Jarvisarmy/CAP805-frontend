7d8dc9a8843c24c26725bf268ef3dd0803ed6bd4
jarvisarmy <1102297439zzd@gmail.com>
2021-04-23 17:32:53 -0400

    zeduo

--- src/components/SignupForm.js
+++ src/components/SignupForm.js
@@ -26,6 +26,7 @@ const SignupForm = () => {
             }; 
             fetch(constant.databaseUrl+'/signupPage', {
                 method: 'POST',
+                credentials: 'include',
                 headers: new Headers({
                     'Content-Type': 'application/json'
                 }),
