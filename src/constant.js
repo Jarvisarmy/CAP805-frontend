9f481ae08a7eb7375cb0a9935dae990dc37253a9
jarvisarmy <1102297439zzd@gmail.com>
2021-04-23 16:59:47 -0400

    update

--- src/constant.js
+++ src/constant.js
@@ -2,8 +2,8 @@
                   
 // headers.append('Authorization', 'Basic ' + base64.encode(username + ":" + password));
 const constant = {
-  //databaseUrl: 'https://guarded-anchorage-82080.herokuapp.com'
+  databaseUrl: 'https://guarded-anchorage-82080.herokuapp.com'
 
-  databaseUrl: 'http://localhost:8080'
+  //databaseUrl: 'http://localhost:8080'
 }
 export default constant;
